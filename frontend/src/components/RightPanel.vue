ab34938f140e925e4b28a6ccbebf498790d14cc3
Dipstick713 <139313088+Dipstick713@users.noreply.github.com>
2025-04-17 18:17:25 +0530

    Add RightPanel

---
+++ frontend/src/components/RightPanel.vue
@@ -0,0 +1,13 @@
+<template>
+    <aside class="w-80 p-4 border-l border-[#253446]">
+      <div class="bg-gradient-to-br from-green-400/100 to-yellow-400/100 p-4 rounded-lg mb-6">
+        
+        <p class="text-sm text-[#0e0f10] font-bold">Plant a new seed of thought.</p>
+        <button class="mt-3 bg-[#0e0f10] text-green-400/100 text-sm px-3 py-1 rounded-lg hover:bg-green-400">
+          Create Post
+        </button>
+      </div>
+  
+    </aside>
+  </template>
+  
