ee47fe7753297f11f2816f43d5294b612a47c814
Dipstick713 <139313088+Dipstick713@users.noreply.github.com>
2025-04-17 18:17:00 +0530

    Add Sidebar

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { 
+  House,
+  FolderGit2,
+  Users,
+  User,
+  GitFork,
+  LogOut 
+ } from 'lucide-vue-next';
+const logout = () => {
+  // Add your logout logic here
+  console.log('Logging out...')
+}
+</script>
+
+
+<template>
+  <aside class="w-80 p-6 flex flex-col justify-between border-r border-[#253344]">
+    <div class="font-roboto">
+      <h2 class="flex text-2xl font-bold h-25">
+        <GitFork class="mx-2"/>
+        Forked</h2>
+      <nav class="space-y-12 pt-30 text-xl text-neutral-300">
+        <a href="#" class="flex w-35 space-y-5 hover:text-white rounded-full h-12 items-center hover:bg-zinc-800 transition-colors duration-200"><House class="mx-4"/>Home</a>
+        <a href="#" class="flex w-46 space-y-5 hover:text-white rounded-full h-12 items-center hover:bg-zinc-800 transition-colors duration-200"><FolderGit2 class="mx-4"/>New Posts</a>
+        <a href="#" class="flex w-45 space-y-5 hover:text-white rounded-full h-12 items-center hover:bg-zinc-800 transition-colors duration-200"><Users class="mx-4"/>Following</a>
+        <a href="#" class="flex w-37 space-y-5 hover:text-white rounded-full h-12 items-center hover:bg-zinc-800 transition-colors duration-200"> <User class="mx-4"/>Profile</a>
+      </nav>
+    </div>
+
+    <!-- User Profile Section -->
+    <div class="flex items-center justify-between p-3 rounded-lg transition cursor-pointer">
+      <div class="flex items-center space-x-3">
+        <img 
+          src="https://randomuser.me/api/portraits/men/1.jpg" 
+          class="w-10 h-10 rounded-full object-cover border border-neutral-600"
+          alt="User avatar"
+        >
+        <div class="space-y-1">
+          <p class="font-medium text-white">John Doe</p>
+          <p class="text-sm text-neutral-400">john@example.com</p>
+        </div>
+      </div>
+      <button 
+        class="text-neutral-400 pb-5 hover:text-red-500 rounded-full transition"
+        @click="logout"
+      >
+        <LogOut/>
+      </button>
+    </div>
+  </aside>
+</template>
+
