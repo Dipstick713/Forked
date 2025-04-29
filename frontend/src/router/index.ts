91cc670ac7c13e02dd54e961dc1b1a799003cc17
Dipstick713 <139313088+Dipstick713@users.noreply.github.com>
2025-04-29 23:57:46 +0530

    Update router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,4 +1,8 @@
-/*import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import PostView from '@/views/PostView.vue'
+import ProfileView from '@/views/ProfileView.vue'
+import FollowView from '@/views/FollowView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +12,27 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/post',
+      name: 'post',
+      component: PostView,
+    },
+    {
+      path: '/user/followers',
+      name: 'followers',
+      component: FollowView
+    },
+    {
+      path: '/user/following',
+      name: 'following',
+      component: FollowView
+    },
+    {
+      path: '/:user',
+      name: 'profile',
+      component: ProfileView
+    }
   ],
 })
 
-export default router*/
+export default router
