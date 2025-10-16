11ef1faacf757221ada0ac046e73751251652951
Dipstick713 <139313088+Dipstick713@users.noreply.github.com>
2025-10-16 22:40:02 +0530

    Fix import error

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
 const connectDB = require('./config/database');
-require('dotenv').config({ path: './backend/.env' });
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
